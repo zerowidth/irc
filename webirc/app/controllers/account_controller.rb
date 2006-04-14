f6ff6a0a066464b7a69bc6f17d0827ded2847f1f
nathan <nathan@0bcff365-60cb-4b90-ba5c-a6ecd6f37746>
2006-04-14 04:47:06 +0000

    Added initial user model and account controller (from acts_as_authenticated plugin)

---
+++ webirc/app/controllers/account_controller.rb
@@ -0,0 +1,33 @@
+class AccountController < ApplicationController
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  include AuthenticatedSystem
+
+  # say something nice, you goof!  something sweet.
+  def index
+    redirect_to(:action => 'signup') unless logged_in? or User.count > 0
+  end
+
+  def login
+    return unless request.post?
+    self.current_user = User.authenticate(params[:login], params[:password])
+    if current_user
+      redirect_back_or_default(:controller => '/account', :action => 'index')
+      flash[:notice] = "Logged in successfully"
+    end
+  end
+
+  def signup
+    @user = User.new(params[:user])
+    return unless request.post?
+    if @user.save
+      redirect_back_or_default(:controller => '/account', :action => 'index')
+      flash[:notice] = "Thanks for signing up!"
+    end
+  end
+  
+  def logout
+    self.current_user = nil
+    flash[:notice] = "You have been logged out."
+    redirect_back_or_default(:controller => '/account', :action => 'index')
+  end
+end
