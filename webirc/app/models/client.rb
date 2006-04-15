4c6243f49eea96654804d558c6520ab91a9a3f09
nathan <nathan@0bcff365-60cb-4b90-ba5c-a6ecd6f37746>
2006-04-15 14:11:28 +0000

    Added initial client model. No database table associated, this will use DRb to communicate with a botmaster.

---
+++ webirc/app/models/client.rb
@@ -0,0 +1,7 @@
+class Client
+
+  # this will connect to a DRb server and perform operations against a client.
+  
+
+
+end
---
+++ webirc/test/unit/client_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+require 'drb'
+
+class ClientTest < Test::Unit::TestCase
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
